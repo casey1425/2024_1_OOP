cca1d5d27d7fef7144da673c443f4fba2f2cd19b
casey1425 <leedh1425@naver.com>
2024-04-03 13:50:14 +0900

    Edit Project5B

--- 실습과제/Project5B.java
+++ 실습과제/Project5B.java
@@ -2,42 +2,27 @@ import java.util.Scanner;
 
 public class Project5B {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("양의 정수를 입력하세요: ");
-        int num = sc.nextInt();
-
-        int num1 = num1(num);
-        int num2 = num2(num);
-
-        System.out.println("반복을 사용한 짝수 숫자의 개수: " + num1);
-        System.out.println("재귀를 사용한 짝수 숫자의 개수: " + num2);
+        Scanner sc=new Scanner(System.in);
+        System.out.print("정수를 입력하세요 : ");
+        int num=sc.nextInt();
+        System.out.println("반복결과 : "+sum1(num));
+        System.out.println("재귀결과 : "+sum2(num));
     }
-
-    //반복을 이용
-    public static int num1(int num) {
-        int count = 0;
+    //반복문
+    public static int sum1(int num) {
+        int sum = 0;
         while (num > 0) {
-            int digit = num % 10;
-            if (digit % 2 == 0) {
-                count++;
-            }
+            sum += num % 10;
             num /= 10;
         }
-        return count;
+        return sum;
     }
-
-    //재귀를 이용
-    public static int num2(int num) {
+    //재귀메소드
+    public static int sum2(int num) {
         if (num < 10) {
-            return num % 2 == 0 ? 1 : 0;
+            return num;
         } else {
-            int lastDigit = num % 10;
-            int remaining = num / 10;
-            if (lastDigit % 2 == 0) {
-                return num2(remaining) + 1;
-            } else {
-                return num2(remaining);
-            }
+            return num % 10 + sum2(num / 10);
         }
     }
 }
